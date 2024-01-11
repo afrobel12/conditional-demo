ffaf7be783d0e4f28046870f849b20e6c4a5e8d2
abel <abrahaabel18@gmail.com>
2024-01-11 19:37:01 +0000

    conditional

---
+++ java.js
@@ -0,0 +1,28 @@
+console.log("hello Abel!")
+
+ let seatNumber=50
+     numberOfPeoples=30
+
+ if( seatNumber > numberOfPeoples){ 
+  console.log("There is extra seat in the hall")
+}
+
+  else if( seatNumber == numberOfPeoples){
+    console.log("The hall is full")
+  }
+
+let age =17
+   movieRating="PG"
+
+   if (age > 17) {
+    console.log("you can watch any movie")
+   }
+   else if ( age === 17 ){
+     
+     console.log("you can't watch the movie")      
+   }
+
+  
+ 
+
+
